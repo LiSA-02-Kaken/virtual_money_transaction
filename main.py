8c5a031250b76a201104e1ce5bded350bc36098f
Naxii <github@naxii.net>
2022-04-23 23:13:57 +0900

    fixed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import Depends, FastAPI
 from fastapi.security import OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from auth import get_user, get_user_from_refresh_token, generate_tokens, authenticate
+from models import Shop
 
 app = FastAPI()
 
@@ -23,7 +24,9 @@ async def login(form: OAuth2PasswordRequestForm = Depends()):
     user = authenticate(form.username, form.password)
     return generate_tokens(user.id)
 
-#@app.post("/transaction/pay")
+@app.post("/transaction/pay")
+async def payment(user: User = Depends(get_user)):
+    user_balance = user.balance
 
 @app.get("/refresh_token", response_model=Token)
 async def refresh_token(current_user: User = Depends(get_user_from_refresh_token)):
