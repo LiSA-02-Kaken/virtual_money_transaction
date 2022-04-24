f0a1edce54e62671eb26e75155ea64ba4dc5fdea
Naxii <github@naxii.net>
2022-04-24 22:30:00 +0900

    決済ログのモデル作成

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
-from peewee import SqliteDatabase, Model, AutoField, CharField, TextField, IntegerField
+from datetime import datetime, timedelta
+from xmlrpc.client import DateTime
+from peewee import SqliteDatabase, Model, AutoField, CharField, TextField, IntegerField, DateTimeField
 from pass_hexdigest import gen_hash
 
 db = SqliteDatabase('db.sqlite3')
@@ -12,11 +14,22 @@ class User(Model):
     balance = IntegerField(default=0)
     class Meta:
         database = db
+
 class Shop(Model):
     id = AutoField(primary_key=True)
     name = TextField()
     shopid = TextField()
     balance = IntegerField(default=0)
+    class Meta:
+        database = db
+class Log(Model):
+    id = AutoField(primary_key=True)
+    session = CharField(16)
+    user = CharField(100)
+    shop = TextField()
+    balance = IntegerField()
+    time = DateTimeField()
+    option = CharField(50, null=True)
     class Meta:
         database = db
 
@@ -25,6 +38,7 @@ if __name__ == "__main__":
 
     db.create_tables([User])
     db.create_tables([Shop])
+    db.create_tables([Log])
 
     Shop.create(name='lisa', shopid="u7ab4")
     User.create(name='naxii', password=gen_hash("aiueo"))
